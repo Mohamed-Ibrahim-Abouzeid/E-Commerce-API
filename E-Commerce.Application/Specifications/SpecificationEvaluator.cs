a56f6e946a993b37a0e7aef0b03927acbffdb5b2
mohamed8777 <hamohamo8uuuu@gmail.com>
2026-06-28 14:18:26 +0200

    Session3

---
+++ E-Commerce.Application/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Domain.Common;
+using E_Commerce.Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce.Application.Specifications
+{
+    public class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> CreateQuery<TEntity, TKey>(IQueryable<TEntity> inputQuery, ISpecifications<TEntity, TKey> specifications) where TEntity : BaseEntity<TKey> {
+          var query= inputQuery;
+            if (specifications.Criteria != null) 
+            {
+                query = query.Where(specifications.Criteria);
+            }
+
+            if (specifications.IncludeExpressions.Any()) {
+                query = specifications.IncludeExpressions.Aggregate(query, (current, NextExp) => current.Include(NextExp));
+            }
+            return query;
+            
+        }
+    }
+}
